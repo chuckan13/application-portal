8b16b022e3885f58904cbd437567e03919a9d6b4
Christine Hu <huchristine.17@gmail.com>
2018-11-24 21:33:03 -0500

    shifting to react-bootstrap

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,16 @@ import './App.css';
 import Apply from './Apply.js';
 import Admin from './Admin.js';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { Button } from 'react-bootstrap';
+import { ButtonToolbar } from 'react-bootstrap';
+import { Navbar } from 'react-bootstrap';
+import { Nav } from 'react-bootstrap';
+import { NavItem } from 'react-bootstrap';
+import { NavDropdown } from 'react-bootstrap';
+import { MenuItem } from 'react-bootstrap';
+import { Grid } from 'react-bootstrap';
+import { Row } from 'react-bootstrap';
+import { Col } from 'react-bootstrap';
 
 class App extends Component {
 
@@ -19,13 +29,26 @@ class App extends Component {
     return (
     <Router>
       <div>
-      <ul>
-        <li><a>Home</a></li>
-        <li><a>Teams</a></li>
-        <li id="login"><a>Login</a></li>
-    </ul>
-      <Route path="/apply" component={Apply} />
-       <Route path="/admin" component={Admin} />
+        <Navbar>
+          <Navbar.Header>
+            <Navbar.Brand>
+              <a>Princeton E-Club</a>
+            </Navbar.Brand>
+          </Navbar.Header>
+          <Nav pullRight>
+            <NavItem eventKey={2} pullRight>Login</NavItem>
+            <NavDropdown eventKey={3} title="Teams" id="basic-nav-dropdown">
+              <MenuItem eventKey={3.1}>Team 1</MenuItem>
+              <MenuItem eventKey={3.2}>Team 2</MenuItem>
+              <MenuItem eventKey={3.3}>Team 3</MenuItem>
+            </NavDropdown>
+          </Nav>
+        </Navbar>
+
+        <Button bsStyle="primary" bsSize="large" id="login-button">Log In</Button>
+
+        <Route path="/apply" component={Apply} />
+        <Route path="/admin" component={Admin} />
       </div> 
     </Router>
   );
