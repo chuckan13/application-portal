7f2eb7e0b855862f69592c39bbf741b2c8a6bb71
PlatGod <an.charlie@gmail.com>
2020-06-09 00:18:53 -0400

    added question model

--- src/main/java/com/example/Question.java
+++ src/main/java/com/example/Question.java
@@ -1,5 +1,43 @@
 package com.example;
 
-public class Question {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Column;
 
+@Entity
+public class Question {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private long id;
+
+    private String text;
+    private long teamId;
+
+    public String getText() {
+        return this.text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public long getTeamId() {
+        return this.teamId;
+    }
+
+    public void setTeamId(long teamId) {
+        this.teamId = teamId;
+    }
+
+    public long getId() {
+        return this.id;
+    }
+
+    public void updateParameters(Question other) {
+        this.text = other.getText();
+        this.teamId = other.getTeamId();
+    }
 }
